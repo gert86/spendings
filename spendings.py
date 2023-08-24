1cd2578e446da2ee685497f83a3d56ec0c74ab5b
Gert Hutter <gert.hutter@reactivereality.com>
2023-08-24 10:04:02 +0200

    Improved and simplified code. Fixed detail display with multiple csv files.

--- spendings.py
+++ spendings.py
@@ -88,7 +88,7 @@ def tile_figures(cols=3, rows=2, screen_rect=None, tile_offsets=None):
         set_figure_geometry(fig, backend, x, y, w, h)
         #print(f"Figure {i}: x={x}, y={y}, w={w}, h={h}")
 
-def test(n_figs=10, backend="TKAgg", **kwargs):
+def testTiling(n_figs=10, backend="TKAgg", **kwargs):
     mpl.use(backend)
     plt.close("all")
     for i in range(n_figs):
@@ -96,8 +96,41 @@ def test(n_figs=10, backend="TKAgg", **kwargs):
     tile_figures(**kwargs)
     plt.show()
 
+def determineCategory(subject, amount, details_container):
+  categories = categories_revenue if amount > 0.0 else categories_spending
+  for title,colorRegexTuple in categories.items():
+    regexes = colorRegexTuple["regex"]
+    for regex in regexes:
+      if re.match(regex, subject, re.IGNORECASE):
+        if len(str(regex)) > 2:
+          details_container.setdefault(str(regex),[]).append(amount)   # store amounts per regex
+        return title      
+  fallback = list(categories_revenue.keys())[-1]
+  return fallback
+    
+def cleanupData(df):
+  # Convert the 'Amount' column to numeric data type
+  df['Amount'] = pd.to_numeric(df['Amount'].str.replace('.', '', regex=False)
+                                           .str.replace(',', '.', regex=False), errors='coerce')      
+
+  # TODO: consider to remove special characters, etc...to make regex easier, e.g.:
+  #df['Subject'] = df.apply(lambda row: row['Subject'].split("|")[-1].strip(), axis=1)
+
+  return df
+
+def getSumAndDetailText(data_frame, cat):
+  le_sum = data_frame[data_frame['Category']==cat]['Amount'].abs().sum()
+  text=f"{cat}: {round(le_sum,2)}€\n---------------------------------------------\n"
+  for r in categories_all[cat]["regex"]:
+    if r in details.keys():
+      text += r + ": " + str(round(abs(sum(details[r])), 2)) + "€\n"   
+  return le_sum, text
 
 
+
+##############################################################################################
+#      MAIN
+##############################################################################################
 REAL_CONFIG_PATH = "./data/config.ini"
 EXAMPLE_CONFIG_PATH = "./data/example_config.ini"
 
@@ -109,91 +142,50 @@ else:
   print(f"\nReading EXAMPLE config from {EXAMPLE_CONFIG_PATH}\n")
   config.read(EXAMPLE_CONFIG_PATH)
 
-
-
-
 COLUMNS = ['Konto','Subject','Date1','Date2','Amount','Currency']
 results = {}
 categories_revenue = json.loads(config['DEFAULT']['categories_revenue'])
 categories_spending= json.loads(config['DEFAULT']['categories_spending'])
 categories_all = {**categories_revenue, **categories_spending}
 
-details = {}
-def determineCategory(subject, amount):
-  categories = categories_revenue if amount > 0.0 else categories_spending
-  for title,colorRegexTuple in categories.items():
-    regexes = colorRegexTuple["regex"]
-    for regex in regexes:
-      if re.match(regex, subject, re.IGNORECASE):
-        if len(str(regex)) > 2:
-          details.setdefault(str(regex),[]).append(amount)   # store amounts per regex
-        return title
-      
-  fallback = list(categories_revenue.keys())[-1]
-  return fallback
-    
-
-def cleanupData(df):
-  # Convert the 'Amount' column to numeric data type
-  df['Amount'] = pd.to_numeric(df['Amount'].str.replace('.', '', regex=False)
-                                           .str.replace(',', '.', regex=False), errors='coerce')      
-
-  # TODO: consider to remove special characters, etc...to make regex easier, e.g.:
-  #df['Subject'] = df.apply(lambda row: row['Subject'].split("|")[-1].strip(), axis=1)
-
-  return df
-
-
 # Get the CSV file path from the command-line argument (specifies a config key)
 config_key = "DEFAULT"  # default file
 if len(sys.argv) >= 2:
   config_key = sys.argv[1]
 
-#csv_file_path = config[config_key]['csv_file_path'].strip("'").strip('"')
 csv_file_paths = config[config_key]['csv_file_paths'].split(",")
 csv_file_paths = [c.strip("'").strip('"').strip('\n').strip(" ") for c in csv_file_paths]
-
-
-
-#csv_file_path = csv_file_paths[1]
-rev_heights = []
-spending_heights = []
 for csv_file_path in csv_file_paths:
 
   # Read the CSV file
   df = pd.read_csv(csv_file_path, names=COLUMNS, sep=';')
   cleanupData(df)
 
-  # Categorize
-  df['Category'] = df.apply(lambda row: determineCategory(str(row['Subject']), float(row['Amount'])), axis=1)
+  # Categorize  
+  details = {}  
+  df['Category'] = df.apply(lambda row: determineCategory(str(row['Subject']), float(row['Amount']), details), axis=1)
 
   # Filter revenues and spendings
   revenues = df[df['Amount'] > 0]
   spendings = df[df['Amount'] < 0]
 
-  # create figure
-  x = ['Incomes '+ str(round(revenues['Amount'].abs().sum())) + "€", 
-      'Spendings '+ str(round(spendings['Amount'].abs().sum())) + "€"]
-
-
-  Ys = []
-  for cat,data in categories_revenue.items():
-    le_sum = revenues[revenues['Category']==cat]['Amount'].abs().sum()
-    rev_heights.append(round(le_sum))
-    if le_sum != 0.0:
-       Ys.append( [le_sum, 0.0, data["color"]] )  
-  for cat,data in categories_spending.items():
-    le_sum = spendings[spendings['Category']==cat]['Amount'].abs().sum()
-    spending_heights.append(round(le_sum))
-    if le_sum != 0.0:
-      Ys.append( [0.0, le_sum, data["color"]] )    
   # stacked bars
   fig, ax = plt.subplots()
   plt.title(f"Incomes and Spendings ({os.path.basename(csv_file_path)})")
+  x = ['Incomes '+ str(round(revenues['Amount'].abs().sum())) + "€", 
+       'Spendings '+ str(round(spendings['Amount'].abs().sum())) + "€"]  
   offset = np.array([0.0, 0.0])
-  for y in Ys:
-    ax.bar(x, y[0:2], bottom = offset, width=0.1, color=y[2])
-    offset = np.add(offset, y[0:2])
+  for cat,data in categories_revenue.items():
+     le_sum, text = getSumAndDetailText(revenues, cat)
+     if le_sum != 0.0:
+        ax.bar(x, [le_sum, 0.0], bottom = offset, width=0.1, color=data["color"], label=text)
+        offset = np.add(offset, [le_sum, 0.0])
+  for cat,data in categories_spending.items():
+     le_sum, text = getSumAndDetailText(spendings, cat)
+     if le_sum != 0.0:
+        ax.bar(x, [0.0, le_sum], bottom = offset, width=0.1, color=data["color"], label=text)
+        offset = np.add(offset, [0.0, le_sum])           
+
   # legend
   legendHandles = []
   for title in categories_all.keys():
@@ -202,10 +194,14 @@ for csv_file_path in csv_file_paths:
       color = categories_all[title]["color"]
       label = title + "(" + str(amount) + "€)"
       legendHandles.append(mpatches.Patch(color=color, label=label))
-  ax.legend(handles=legendHandles)  
-  ax.set_yticks(np.arange(0, 100000, 10000))
+  ax.legend(handles=legendHandles, prop={'size': 6})
+
+  # grid
+  max_y = 50000
+  grid_interval = 10000
+  ax.set_yticks(np.arange(0, max_y+grid_interval, grid_interval))
   ax.grid(axis='y', linestyle='--')
-  ax.set(ylim=(0, 50000))
+  ax.set(ylim=(0, max_y))
   fig.canvas.manager.window.overrideredirect(1)  # remove window frame
 
   cursor = mplcursors.cursor(hover=mplcursors.HoverMode.Transient)
@@ -213,18 +209,7 @@ for csv_file_path in csv_file_paths:
   def on_add(sel):
       x, y, width, height = sel.artist[sel.index].get_bbox().bounds      
       x_index = x+width/2      
-      heights = rev_heights if round(x_index)==0 else spending_heights
-      categories = categories_revenue if round(x_index)==0 else categories_spending
-      stack_index = heights.index(round(height))
-      #print(f"{os.path.basename(csv_file_path)}: Type = {'Revenues' if round(x_index)==0 else 'Spendings'}, Height: {height}")
-      category = list(categories.keys())[stack_index]
-      text=f"{category}: {round(height,2)}€\n---------------------------------------------\n"  
-      if category in categories.keys():
-        regex_list = categories[category]["regex"]
-        for r in regex_list:
-          if r in details.keys():
-            text += r + ": " + str(round(abs(sum(details[r])), 2)) + "€\n"
-
+      text = sel.artist._label
       sel.annotation.set(text=text,
                          anncoords="offset points", ha="left")
       sel.annotation.xy = (x_index, y + height)
